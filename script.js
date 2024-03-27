c45da0c8018ea8316f8413b232ef15638dee768b
Morymirco <morykoulibaly996@gmail.com>
2024-03-27 15:52:02 +0000

    bob

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const btn = document.getElementById("btn");
-const search = document.querySelector(" #input");
-// Ajoutez l'événement click une seule fois
+const search = document.querySelector("#input");
+
 btn.addEventListener("click", () => {
   if (window.innerWidth < 510) {
-    console.log("arrete");
+    console.log("Arrêtez");
     search.style.width = "100%";
     search.style.transition = "width 2s ease-in-out";
-  }
-});
-btn.addEventListener("moveover", () => {
-  if (window.innerWidth < 510) {
-    console.log("arrete");
-    search.style.width = "100%";
-    search.style.transition = "width 2s ease-in-out";
+
+    // Ajouter l'écouteur d'événement change ici
+    search.addEventListener("change", () => {
+      search.style.width = "0px";
+      search.style.transition = "width 2s ease-in-out";
+    });
   }
 });
